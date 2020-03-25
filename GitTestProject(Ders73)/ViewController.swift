b70908154877bb2cd3b646b57b9dd7eed92e1705
ayom <mrosakman@gmail.com>
2020-03-25 20:13:42 +0300

    Initial Commit

---
+++ GitTestProject(Ders73)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject(Ders73)
+//
+//  Created by omersakman on 25.03.2020.
+//  Copyright © 2020 omersakman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
